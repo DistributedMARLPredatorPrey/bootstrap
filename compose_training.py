6eddaf3845fae95ae072bcbe9711c8d1578f4c2b
Luca Fabri <luca.fabri1999@gmail.com>
2024-11-15 11:35:27 +0100

    chore: remove unused files, update compose training script

--- compose_training.py
+++ compose_training.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 
+import argparse
+import sys
 
-pred_prey_services = [f"""
+
+def compose_train(num_env: int):
+    pred_prey_services = [
+        f"""
     predator-prey-service-{i}:
-        image: predator-prey-service #ghcr.io/distributedmarlpredatorprey/predator-prey-service:release-0.2.3
+        image: pred-prey-service #ghcr.io/distributedmarlpredatorprey/predator-prey-service:release-0.2.3
         container_name: predator-prey-service-{i}
-        hostname: predator-prey-service-{i}
+        hostname: predator-prey-{i}
+        restart: always
         depends_on:
             rabbitmq:
                 condition: service_healthy
         environment:
-            PYTHONUNBUFFERED: 1
-            GLOBAL_CONFIG_PATH: /usr/app/config/config.yaml
-            REPLAY_BUFFER_HOST: replay-buffer
-            REPLAY_BUFFER_PORT: 80
-            BROKER_HOST: rabbitmq-broker
+            <<: *common-variables
             REL_PATH: {i}
             RANDOM_SEED: {i}
             MODE: train
@@ -26,20 +28,24 @@ pred_prey_services = [f"""
         volumes:
             - ./config/:/usr/app/config/
             - ../predator-prey-service/:/usr/app/
-    """ for i in range(5)]
+        """
+        for i in range(5)
+    ]
 
-prefix = """
-version: '3'
+    prefix = """x-common-variables: &common-variables
+    GLOBAL_CONFIG_PATH: /usr/app/config/config.yaml
+    PYTHONUNBUFFERED: 1
+    REPLAY_BUFFER_HOST: replay-buffer
+    REPLAY_BUFFER_PORT: 80
+    BROKER_HOST: rabbitmq-broker
+    PROJECT_ROOT_PATH: /usr/app/
 
 services:
-
     rabbitmq:
         image: rabbitmq:3-management
         container_name: rabbitmq-broker
         hostname: rabbitmq-broker
-        ports:
-            - "5672:5672"  # RabbitMQ main port
-            - "15672:15672"  # RabbitMQ management plugin port
+        restart: always
         environment:
             RABBITMQ_HEARTBEAT: 60
         volumes:
@@ -54,54 +60,76 @@ services:
         image: replay-buffer-service #ghcr.io/distributedmarlpredatorprey/replay-buffer-service:release-0.3.0
         container_name: replay-buffer-service
         hostname: replay-buffer
+        restart: always
         environment:
-            PYTHONUNBUFFERED: 1
-            GLOBAL_CONFIG_PATH: /usr/app/config/config.yaml
-            PREDATOR_DATASET_PATH: /usr/app/dataset/predator_data1.csv
-            PREY_DATASET_PATH: /usr/app/dataset/prey_data1.csv
+            <<: *common-variables            
+            DATASET_PATH: /usr/app/dataset/dataset.csv
         volumes:
             - ./config/config.yaml:/usr/app/config/config.yaml
             - ./config/replay-buffer/:/usr/app/dataset/
             - ../replay-buffer-service/:/usr/app/
-"""
-
-learner_dependencies = ""
-for i in range(len(pred_prey_services)):
-    dep = f"""
-            predator-prey-service-{i}:
-                condition: service_healthy
     """
-    learner_dependencies = learner_dependencies + dep
 
-suffix = f"""
+    learner_dependencies = ""
+    for i in range(len(pred_prey_services)):
+        dep = f"""predator-prey-service-{i}:
+                condition: service_healthy
+            """
+        learner_dependencies = learner_dependencies + dep
+
+    suffix = f"""
     learner-service:
         image: learner-service #ghcr.io/distributedmarlpredatorprey/learner-service:main
         container_name: learner-service
-        hostname: pred-learner
+        hostname: learner
+        restart: always
         depends_on:
             rabbitmq:
                 condition: service_healthy
             {learner_dependencies}
         environment:
-            PYTHONUNBUFFERED: 1
-            GLOBAL_CONFIG_PATH: /usr/app/config/config.yaml
-            AGENT_TYPE: predator
-            BATCH_SIZE: 64
-            REPLAY_BUFFER_HOST: replay-buffer
-            REPLAY_BUFFER_PORT: 80
-            BROKER_HOST: rabbitmq-broker
+            <<: *common-variables
+            BATCH_SIZE: 2
         volumes:
             - ./config/config.yaml:/usr/app/config/config.yaml
             - ../learner-service/:/usr/app/
 volumes:
     rabbitmq_data:
-"""
+    """
 
-compose = prefix
-for pred_prey_service in pred_prey_services:
-    compose = compose + pred_prey_service + "\n"
+    compose = prefix
+    for pred_prey_service in pred_prey_services:
+        compose = compose + pred_prey_service
 
-compose = compose + suffix
-print(compose)
+    compose = compose + suffix
+    print(compose)
 
 
+def main():
+    parser = argparse.ArgumentParser(
+        description="Creates a Docker Compose file to run Distributed MARL Predator-Prey"
+    )
+    parser.add_argument(
+        "--num_env",
+        type=str,
+        required=True,
+        help="Number of environments to run in parallel.",
+    )
+    args = parser.parse_args()
+    compose_train(args.num_env)
+    print(
+        "-----------------------------------------------------------------------------------",
+        file=sys.stderr,
+    )
+    print(
+        f"A Docker compose file content is generated for training {args.num_env} environments in parallel.",
+        file=sys.stderr,
+    )
+    print(
+        "-----------------------------------------------------------------------------------",
+        file=sys.stderr,
+    )
+
+
+if __name__ == "__main__":
+    main()
