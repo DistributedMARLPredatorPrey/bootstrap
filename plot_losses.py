34c34cea0c7eccb070d9bcfb595efd4579e356c9
Luca Fabri <luca.fabri1999@gmail.com>
2024-11-19 11:50:53 +0100

    chore: rename x axis

--- plot_losses.py
+++ plot_losses.py
@@ -10,7 +10,7 @@ df_prey_losses = pd.read_csv("./data/learner_service/df_prey_losses.csv", index_
 
 # Print the results
 def plot_critic_loss():
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))  # 1 row, 2 columns
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(14, 6))
 
     for title, losses, ax in [("Predator Critic Loss", df_pred_losses["pred_loss"], ax1), 
                             ("Prey Critic Loss", df_prey_losses["prey_loss"], ax2)]:
@@ -35,7 +35,7 @@ def plot_critic_loss():
             color="red",
             label=f"Regression line (slope: {slope:.4f}, p-value: {p_value:.4f})",
         )
-        ax.set_xlabel("Episodes")
+        ax.set_xlabel("Iteration")
         ax.set_ylabel("Loss")
         ax.set_title(title)
 
